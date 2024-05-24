db2a86fdd54941abd5a3b799e5558f3156f3e5c0
v()id <MikeGraham.295@gmail.com>
2024-05-24 13:45:07 +1200

    Add files via upload

---
+++ GoatTigerGame/src/Goat.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.util.Random;
+
+public class Goat {
+    public int x, y;
+    private int boxIndex, posOffset;
+
+    public Goat(int boxIndex) {
+        this.boxIndex = boxIndex;
+        this.x = GameBoard.getBoxes().get(boxIndex).x;
+        this.y = GameBoard.getBoxes().get(boxIndex).y;
+        this.posOffset = new Random().nextInt(-10, 10);
+    }
+
+    public boolean containsMouse(int mouseX, int mouseY, int boxSize) {
+        return new Rectangle(x, y, boxSize, boxSize).contains(mouseX, mouseY);
+    }
+
+    public int getPosOffset() { return posOffset; }
+}
